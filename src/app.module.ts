e5fb1eb7daca1f2d9a4550290ccc12927bd0f6c8
Willian Oliveira <willian.dev.tech@gmail.com>
2026-06-21 20:30:24 -0300

    feat(app): register organization access modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from './common/logger/logger.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './modules/health/health.module';
+import { InvitationsModule } from './modules/invitations/invitations.module';
+import { MembersModule } from './modules/members/members.module';
 import { OrganizationsModule } from './modules/organizations/organizations.module';
 
 @Module({
-  imports: [LoggerModule, DatabaseModule, HealthModule, OrganizationsModule],
+  imports: [
+    LoggerModule,
+    DatabaseModule,
+    HealthModule,
+    OrganizationsModule,
+    MembersModule,
+    InvitationsModule,
+  ],
 })
 export class AppModule {}
